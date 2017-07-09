195477ab8410e9dd74324a62e5ec3d596be7f355
taeiim <ti535666@gmail.com>
2017-07-10 08:35:53 +0900

    Calendar with grid view

--- Calendar/Calendar_Grid/app/src/main/java/com/example/parktaeim/calendar_1/AddActivity.java
+++ Calendar/Calendar_Grid/app/src/main/java/com/example/parktaeim/calendar_1/AddActivity.java
@@ -1,8 +1,12 @@
 package com.example.parktaeim.calendar_1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -16,6 +20,26 @@ public class AddActivity extends Activity{
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        Button cancelBtn = (Button) findViewById(R.id.cancelButton);
+        Button okBtn = (Button) findViewById(R.id.okButton);
+
+        cancelBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        okBtn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
