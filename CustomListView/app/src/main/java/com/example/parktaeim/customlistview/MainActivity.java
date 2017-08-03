eb2cc4a3dd9da4f90a8cf2b3a3c0c12af578e35d
taeiim <ti535666@gmail.com>
2017-08-03 21:23:50 +0900

    [Android] Implementing comments

--- CustomListView/app/src/main/java/com/example/parktaeim/customlistview/MainActivity.java
+++ CustomListView/app/src/main/java/com/example/parktaeim/customlistview/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.parktaeim.customlistview;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private ArrayList<String> arrayList;
+    private EditText commentEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mListView = (ListView) findViewById(R.id.listView);
 
@@ -20,8 +28,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void dataSetting(){
-        MyAdapter myAdapter = new MyAdapter();
+        final MyAdapter myAdapter = new MyAdapter();
 
+        commentEdit=(EditText) findViewById(R.id.commentEditText);
+        Button addBtn = (Button) findViewById(R.id.commentAddBtn);
+
+        arrayList= new ArrayList<>();
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newComment = commentEdit.getText().toString();
+                arrayList.add(newComment);
+                myAdapter.notifyDataSetChanged();
+            }
+        });
         for(int i=0;i<10;i++){
             myAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(),R.drawable.profile_img),"name "+i, "contents "+i);
 
