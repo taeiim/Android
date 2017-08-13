5ec3eec75c1008a6cbff8e02a58b7c1399c4eb57
taeiim <ti535666@gmail.com>
2017-08-13 23:06:04 +0900

    search toolbar

--- FreeBoard_2/app/src/main/java/com/example/parktaeim/freeboard_2/Activity/MainActivity.java
+++ FreeBoard_2/app/src/main/java/com/example/parktaeim/freeboard_2/Activity/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ import android.widget.TextView;
 import com.example.parktaeim.freeboard_2.Adapter.postListViewAdapter;
 import com.example.parktaeim.freeboard_2.Data.PostsData;
 import com.example.parktaeim.freeboard_2.R;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -24,14 +28,24 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView mListView = null;
     private com.example.parktaeim.freeboard_2.Adapter.postListViewAdapter mAdapter = null;
+    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //툴바할당
-        setSupportActionBar((Toolbar)findViewById(R.id.FreeBoardMainListToolbar));
+        //툴바설정
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_home);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("자유게시판");
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //검색
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
 
 
         mListView = (ListView) findViewById(R.id.postsListView);
@@ -39,11 +53,11 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new postListViewAdapter(this);
         mListView.setAdapter(mAdapter);
 
-       // postsArrayList.add(new PostsData("오늘은 치킨이 먹고싶다","박태임","2017.08.15.","28",null));
+        // postsArrayList.add(new PostsData("오늘은 치킨이 먹고싶다","박태임","2017.08.15.","28",null));
         //이미지 있을때
-        mAdapter.addItem("오늘 축제 꿀잼~~~", "박태임","2017.08.07","1",getResources().getDrawable(R.drawable.thumbnail,null));
+        mAdapter.addItem("오늘 축제 꿀잼~~~", "박태임", "2017.08.07", "1", getResources().getDrawable(R.drawable.thumbnail, null));
         //이미지 null 일때
-        mAdapter.addItem("오늘 축제 꿀잼~~~", "박태임","2017.08.07","18",null);
+        mAdapter.addItem("오늘 축제 꿀잼~~~", "박태임", "2017.08.07", "18", null);
 
 
         //글 누르면 상세페이지로 넘어가기
@@ -51,22 +65,74 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 PostsData mData = mAdapter.mPostsListData.get(position);
-                Intent intent = new Intent(getApplicationContext(),DetailActivity.class);
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
 
-                intent.putExtra("title",mData.title);
-                intent.putExtra("writer",mData.writer);
-                intent.putExtra("date",mData.date);
-                intent.putExtra("views",mData.views);
+                intent.putExtra("title", mData.title);
+                intent.putExtra("writer", mData.writer);
+                intent.putExtra("date", mData.date);
+                intent.putExtra("views", mData.views);
 
                 startActivity(intent);
             }
         });
 
 
+    }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem search_item = menu.findItem(R.id.search_icon);
+        MenuItem writing_item = menu.findItem(R.id.writing_icon);
+
+        searchView.setMenuItem(search_item);
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+        return true;
     }
 
 
 
 
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.writing_icon:
+                Intent intent = new Intent(MainActivity.this,PostingActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
---
+++ FreeBoard_2/app/src/main/java/com/example/parktaeim/freeboard_2/Activity/PostingActivity.java
@@ -0,0 +1,19 @@
+package com.example.parktaeim.freeboard_2.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.parktaeim.freeboard_2.R;
+
+/**
+ * Created by parktaeim on 2017. 8. 13..
+ */
+
+public class PostingActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.postingpage);
+    }
+}
